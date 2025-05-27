6c96c407e9f0ce6f87c359dca69f78ee43b1e0ba
ldickmann <ldickmann12@gmail.com>
2025-05-27 05:32:26 -0300

    FEAT: Linhas de codigos apagados em diversos arquivos, pois os mesmos tinham sido usados de exemplos

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,8 @@
 <template>
-  <ButtonComponent> Click Me </ButtonComponent>
-  <ButtonComponent> Click Me </ButtonComponent>
-  <ButtonComponent> Click Me </ButtonComponent>
-  <ButtonComponent> Click Me </ButtonComponent>
-  <ButtonComponent> Click Me </ButtonComponent>
   <HomeView />
 </template>
 
 <script setup>
-import ButtonComponent from "./components/ButtonComponent.vue";
 import HomeView from "./views/HomeView.vue";
 </script>
 
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,31 +1,5 @@
-<template>
-  <!-- html -->
-  <h1 class="title">Teste</h1>
+<template></template>
 
-  <TextComponent text="Teste" />7
-  <TextComponent
-    text="Teste 2"
-    textClass="text-default" />
+<script setup></script>
 
-  <!-- fim do html -->
-</template>
-
-<script setup>
-import TextComponent from "../components/TextComponent.vue";
-
-/* Vue.js (Que é o Javascript)
- *
- *
- *
- * Fim
- */
-</script>
-
-<style lang="scss" scoped>
-// Aqui é o CSS (SCSS)
-
-.title {
-  color: #ff0000;
-}
-// ---------------------------- FIM DO CSS ----------------------------
-</style>
+<style lang="scss" scoped></style>
