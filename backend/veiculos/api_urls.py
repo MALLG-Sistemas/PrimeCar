6ed16a2a37133a1ff0f66c8ce0b6d6db5d8491ad
ldickmann <ldickmann12@gmail.com>
2025-05-30 21:47:32 -0300

    DOCS: Comentario sobre as urls da api 'endpoint'

--- backend/veiculos/api_urls.py
+++ backend/veiculos/api_urls.py
@@ -2,6 +2,19 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .api_views import ModeloViewSet, CarroViewSet
 
+"""
+Módulo: api_urls
+----------------
+Este módulo configura o roteamento de URL para a aplicação backend 'veiculos'
+usando o DefaultRouter do Django REST Framework. Ele registra dois viewsets com o roteador:
+1. ModeloViewSet, acessível no endpoint 'modelos'.
+2. CarroViewSet, acessível no endpoint 'carros'.
+O DefaultRouter gera automaticamente o conjunto padrão de rotas CRUD para ambos
+os endpoints, que são então incluídos na configuração geral de URL da aplicação.
+Uso:
+Inclua os urlpatterns deste módulo na configuração de URL do seu projeto para habilitar
+os endpoints da API RESTful para os modelos de veículos e carros.
+"""
 router = DefaultRouter()
 router.register(r"modelos", ModeloViewSet, basename="modelo")
 router.register(r"carros", CarroViewSet, basename="carro")
