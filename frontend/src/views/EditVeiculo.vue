f843ae2dd8ab41f3fafd93afe23d4fd6c757eea4
ldickmann <ldickmann12@gmail.com>
2025-06-11 22:16:57 -0300

    FEAT: Inicio codificação pagina editar veiculos

--- frontend/src/views/EditVeiculo.vue
+++ frontend/src/views/EditVeiculo.vue
@@ -1,26 +1,66 @@
 <template>
-  <section class="section-edit-veiculos">
-    <div class="container">
-      <h1 class="title">Edit Veículo</h1>
+  <section class="section-edit">
+    <div class="section-edit__container">
+      <header class="section-edit__header">
+        <h1 class="section-edit__title">Editar/Visualizar Veículo</h1>
+      </header>
+
+      <div class="section-edit__actions">
+        <ButtonComponent
+          class="section-edit__button"
+          size="large"
+          bgColor="#3D5E73"
+          textColor="#FFFFFF"
+          fontSize="11px"
+          @click="$router.push('/add')">
+          Editar Veículo
+        </ButtonComponent>
+        <ButtonComponent
+          class="section-edit__button"
+          size="large"
+          bgColor="#f00"
+          textColor="#FFFFFF"
+          fontSize="11px"
+          @click="$router.push('/veiculos')">
+          Excluir Veículo
+        </ButtonComponent>
+      </div>
     </div>
   </section>
 </template>
 
-<script setup></script>
+<script setup>
+import ButtonComponent from "../components/ButtonComponent.vue";
+</script>
 
 <style lang="scss" scoped>
 @use "../styles/variables" as *;
+.section-edit {
+  &__container {
+    display: flex;
+    justify-content: space-between;
 
-.section-edit-veiculos {
-  .container {
-    padding: 20px;
+    padding: 44px 22px;
   }
 
-  .title {
-    font-size: 2rem;
-    color: #333;
-    text-align: center;
-    margin-top: 20px;
+  &__header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+  }
+
+  &__title {
+    font-family: $font-primary;
+    font-size: 29px;
+    font-weight: 500;
+    color: $color-text-secondary;
+  }
+
+  &__actions {
+    display: flex;
+    align-items: center;
+    gap: 20px;
   }
 }
 </style>
